c3b6fde8fe465a84a10361012ed051110ba0bf0b
Bezoru <c0mpass0max@gmail.com>
2023-01-29 22:44:54 +0700

    Clear master branch

--- src/main/java/ru/praktikum/src/RentTerms.java
+++
@@ -1,7 +0,0 @@
-package ru.praktikum.src;
-
-public class RentTerms {
-    public static final String ONE_DAY= "сутки";
-    public static final String TWO_DAYS = "двое суток";
-    public static final String THREE_DAYS = "трое суток";
-}
--- src/main/java/ru/praktikum/src/ScooterTypes.java
+++
@@ -1,6 +0,0 @@
-package ru.praktikum.src;
-
-public class ScooterTypes {
-    public static final String BLACK_SCOOTER= "black";
-    public static final String GRAY_SCOOTER = "grey";
-}
